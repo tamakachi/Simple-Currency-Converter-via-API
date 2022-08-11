5452fa99d203f7a791db1855a4156e82500bd439
Kent <kenttechnical@gmail.com>
2022-08-11 15:51:39 +0200

    formatting

--- script.js
+++ script.js
@@ -5,30 +5,24 @@ const convertButton = document.querySelector("#convert")
 const equals = document.querySelector("#equals")
 
 
-const convert = async (base,target,amount) =>{
-
-
+const convert = async (base, target, amount) => {
 
     const request = await fetch(`https://v6.exchangerate-api.com/v6/eb7ac863741dbb33b8f05b43/pair/${base}/${target}/${amount}`)
     return request.json()
 
 }
 
-
-
-convertButton.addEventListener("click",()=>{
-
+convertButton.addEventListener("click", () => {
 
     const fromCurrency = basePair.options[basePair.selectedIndex].value
     const toCurrency = targetPair.options[targetPair.selectedIndex].value
     const fromAmount = Number(amount.value)
 
-    convert(fromCurrency,toCurrency,fromAmount)
-    .then(data => {updateUI(data)})
+    convert(fromCurrency, toCurrency, fromAmount)
+        .then(data => { updateUI(data) })
 })
 
-const updateUI = (data) =>{
+const updateUI = (data) => {
     console.log(data)
     equals.value = data.conversion_result
-    
 }
